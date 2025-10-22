a9f9a14ec5d004dc7d83f8b87512dab1c4353f62
Faseeh <faseehmohammed2002@gmail.com>
2025-10-22 20:34:42 +0400

    filter for stable angle

--- 1_SensorTest.ino/1_SensorTest.ino.ino
+++ 1_SensorTest.ino/1_SensorTest.ino.ino
@@ -1,8 +1,12 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include <math.h> 
 
 Adafruit_MPU6050 mpu;
+
+float anglePitch = 0; 
+unsigned long timer;
 
 void setup() {
   Serial.begin(115200);
@@ -22,28 +26,28 @@ void setup() {
 
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-
+  
+  timer = micros(); 
+  Serial.println("---------------------------------");
+  Serial.println("Starting filter. Open Serial Plotter to see the angle.");
 }
 
 void loop() {
   sensors_event_t a, g, temp;
-  
   mpu.getEvent(&a, &g, &temp);
 
-  Serial.print("Accel X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.println(a.acceleration.z);
 
-  Serial.print("Gyro X: ");
-  Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
-  Serial.println(g.gyro.z);
+  unsigned long now = micros();
+  float dt = (now - timer) / 1000000.0; 
+  timer = now;
 
-  Serial.println("---------------------------------");
-  delay(100); 
+  float pitchAcc = atan2(a.acceleration.y, a.acceleration.z) * 180.0 / M_PI;
+
+  float gyroRate = g.gyro.x * 180.0 / M_PI;
+  
+  anglePitch = 0.98 * (anglePitch + gyroRate * dt) + 0.02 * pitchAcc;
+
+  Serial.println(anglePitch);
+  
+  delay(10); 
 }
