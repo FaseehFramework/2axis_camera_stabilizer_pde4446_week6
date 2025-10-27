23e0a3ff95c855aa96c9ad7871e5d1620adf2040
Faseeh <faseehmohammed2002@gmail.com>
2025-10-27 12:57:33 +0400

    Yaw, Pitch, and Roll angles

--- 3_DMP_SensorTest/3_DMP_SensorTest.ino
+++ 3_DMP_SensorTest/3_DMP_SensorTest.ino
@@ -6,38 +6,39 @@
 
 MPU6050 mpu;
 
-bool dmpReady = false;  
-uint8_t mpuIntStatus;   
-uint8_t devStatus;      
-uint16_t packetSize;    
-uint16_t fifoCount;     
-uint8_t fifoBuffer[64]; 
+bool dmpReady = false;
+uint8_t mpuIntStatus;
+uint8_t devStatus;
+uint16_t packetSize;
+uint16_t fifoCount;
+uint8_t fifoBuffer[64];
 
-#define INTERRUPT_PIN 2  
-volatile bool mpuInterrupt = false;     
+Quaternion q;           // [w, x, y, z] quaternion container
+VectorFloat gravity;    // [x, y, z] gravity vector
+float ypr[3];           // [yaw, pitch, roll] yaw/pitch/roll container
+
+#define INTERRUPT_PIN 2
+volatile bool mpuInterrupt = false;
 void dmpDataReady() {
     mpuInterrupt = true;
 }
 
-  
 void setup() {
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         Wire.begin();
-        Wire.setClock(400000); 
+        Wire.setClock(400000);
     #endif
-    Serial.begin(115200); 
+    Serial.begin(115200);
     
     Serial.println(F("I2C device setup"));
     mpu.initialize();
     pinMode(INTERRUPT_PIN, INPUT);
 
-    Serial.println(F("device connections"));
-    if (mpu.testConnection()) {
-        Serial.println(F("MPU6050 connection successful"));
-    } else {
+    if (!mpu.testConnection()) {
         Serial.println(F("MPU6050 connection failed"));
         while(1);
     }
+    Serial.println(F("MPU6050 connection successful"));
 
     Serial.println(F("starting DMP"));
     devStatus = mpu.dmpInitialize();
@@ -46,6 +47,7 @@ void setup() {
         Serial.println(F("Enabling DMP"));
         mpu.setDMPEnabled(true);
 
+        Serial.println(F("Enabling interrupt detection..."));
         attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
         mpuIntStatus = mpu.getIntStatus();
 
@@ -67,7 +69,6 @@ void loop() {
 
     mpuInterrupt = false;
     mpuIntStatus = mpu.getIntStatus();
-
     fifoCount = mpu.getFIFOCount();
 
     if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
@@ -76,11 +77,22 @@ void loop() {
 
     } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
         while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
         mpu.getFIFOBytes(fifoBuffer, packetSize);
-        
         fifoCount -= packetSize;
 
-        Serial.println("Packet received!");
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+
+        float yaw   = ypr[0] * 180 / M_PI;
+        float pitch = ypr[1] * 180 / M_PI;
+        float roll  = ypr[2] * 180 / M_PI;
+
+        Serial.print("YPR: ");
+        Serial.print(yaw);
+        Serial.print(", ");
+        Serial.print(pitch);
+        Serial.print(", ");
+        Serial.println(roll);
     }
 }
