dfd6329526bc9ac40861e19e5fce69eec71db3af
Faseeh <faseehmohammed2002@gmail.com>
2025-10-27 12:41:41 +0400

    Added i2cdevlib and MPU-6050 DMP example

---
+++ 3_DMP_SensorTest/3_DMP_SensorTest.ino
@@ -0,0 +1,86 @@
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+MPU6050 mpu;
+
+bool dmpReady = false;  
+uint8_t mpuIntStatus;   
+uint8_t devStatus;      
+uint16_t packetSize;    
+uint16_t fifoCount;     
+uint8_t fifoBuffer[64]; 
+
+#define INTERRUPT_PIN 2  
+volatile bool mpuInterrupt = false;     
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+  
+void setup() {
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); 
+    #endif
+    Serial.begin(115200); 
+    
+    Serial.println(F("I2C device setup"));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+
+    Serial.println(F("device connections"));
+    if (mpu.testConnection()) {
+        Serial.println(F("MPU6050 connection successful"));
+    } else {
+        Serial.println(F("MPU6050 connection failed"));
+        while(1);
+    }
+
+    Serial.println(F("starting DMP"));
+    devStatus = mpu.dmpInitialize();
+
+    if (devStatus == 0) {
+        Serial.println(F("Enabling DMP"));
+        mpu.setDMPEnabled(true);
+
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        dmpReady = true;
+        packetSize = mpu.dmpGetFIFOPacketSize();
+        Serial.println(F("DMP ready"));
+    } else {
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+}
+
+void loop() {
+    if (!dmpReady) return;
+
+    while (!mpuInterrupt && fifoCount < packetSize) {
+    }
+
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    fifoCount = mpu.getFIFOCount();
+
+    if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        fifoCount -= packetSize;
+
+        Serial.println("Packet received!");
+    }
+}
