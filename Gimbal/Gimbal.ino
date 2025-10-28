ef42684a2b1a5d3cfb4294705dd2a1623188354c
Faseeh <faseehmohammed2002@gmail.com>
2025-10-28 13:03:50 +0400

    DMP sensor + servo control

---
+++ Gimbal/Gimbal.ino
@@ -0,0 +1,105 @@
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include <Servo.h>
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+MPU6050 mpu;
+
+bool dmpReady = false;
+uint8_t mpuIntStatus;
+uint8_t devStatus;
+uint16_t packetSize;
+uint16_t fifoCount;
+uint8_t fifoBuffer[64];
+
+Quaternion q;
+VectorFloat gravity;
+float ypr[3]; 
+Servo servo0; // Yaw
+Servo servo1; // Pitch (Tilt)
+Servo servo2; // Roll
+
+#define INTERRUPT_PIN 2
+volatile bool mpuInterrupt = false;
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+void setup() {
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000);
+    #endif
+    Serial.begin(38400); 
+    
+    Serial.println(F("Initializing I2C devices"));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+
+    if (!mpu.testConnection()) {
+        Serial.println(F("MPU6050 connection failed"));
+        while(1);
+    }
+    Serial.println(F("MPU6050 connection successful"));
+
+    Serial.println(F("Initializing DMP"));
+    devStatus = mpu.dmpInitialize();
+
+    mpu.setXGyroOffset(17);
+    mpu.setYGyroOffset(-69);
+    mpu.setZGyroOffset(27);
+    mpu.setZAccelOffset(1551);
+
+    if (devStatus == 0) {
+        Serial.println(F("Enabling DMP"));
+        mpu.setDMPEnabled(true);
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+        dmpReady = true;
+        packetSize = mpu.dmpGetFIFOPacketSize();
+        Serial.println(F("DMP ready!"));
+    } else {
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+
+    servo0.attach(10);
+    servo1.attach(9);
+    servo2.attach(8);
+    servo0.write(90);
+    servo1.write(90);
+    servo2.write(90);
+}
+
+void loop() {
+    if (!dmpReady) return;
+
+    while (!mpuInterrupt && fifoCount < packetSize) {
+    }
+
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+    fifoCount = mpu.getFIFOCount();
+
+    if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        fifoCount -= packetSize;
+
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+
+        float yaw   = ypr[0] * 180 / M_PI;
+        float pitch = ypr[1] * 180 / M_PI;
+        float roll  = ypr[2] * 180 / M_PI;
+
+    }
+}
