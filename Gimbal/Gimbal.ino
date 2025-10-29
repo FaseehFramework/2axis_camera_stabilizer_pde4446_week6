903807f5277f656d4eba508346f413960c79ee97
Faseeh <faseehmohammed2002@gmail.com>
2025-10-29 11:38:22 +0400

    set mpu offset , mapped pitch&roll , code cleanup

--- Gimbal/Gimbal.ino
+++ Gimbal/Gimbal.ino
@@ -1,112 +1,117 @@
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
-#include <Servo.h>
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    #include "Wire.h"
+#include "Wire.h"
 #endif
-
+#include <Servo.h>
 MPU6050 mpu;
+Servo servo0;
+Servo servo1;
+Servo servo2;
+float correct;
+int j = 0;
 
-bool dmpReady = false;
-uint8_t mpuIntStatus;
-uint8_t devStatus;
-uint16_t packetSize;
-uint16_t fifoCount;
-uint8_t fifoBuffer[64];
+#define OUTPUT_READABLE_YAWPITCHROLL
+#define INTERRUPT_PIN 2  
+bool blinkState = false;
+bool dmpReady = false;  
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
 
-Quaternion q;
-VectorFloat gravity;
-float ypr[3]; // [yaw, pitch, roll]
-
-Servo servo0; // Yaw
-Servo servo1; // Pitch (Tilt)
-Servo servo2; // Roll
-
-float correct = 0; 
-int j = 0; 
-
-#define INTERRUPT_PIN 2
-volatile bool mpuInterrupt = false;
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
 void dmpDataReady() {
-    mpuInterrupt = true;
+  mpuInterrupt = true;
 }
 
 void setup() {
-    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-        Wire.begin();
-        Wire.setClock(400000);
-    #endif
-    Serial.begin(38400); 
-    
-    mpu.initialize();
-    pinMode(INTERRUPT_PIN, INPUT);
-    devStatus = mpu.dmpInitialize();
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+  Wire.setClock(400000); 
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+  Serial.begin(38400);
+  while (!Serial); 
 
-    mpu.setXGyroOffset(17);
-    mpu.setYGyroOffset(-69);
-    mpu.setZGyroOffset(27);
-    mpu.setZAccelOffset(1551);
-
-    if (devStatus == 0) {
-        mpu.setDMPEnabled(true);
-        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
-        dmpReady = true;
-        packetSize = mpu.dmpGetFIFOPacketSize();
-    } else {
-    }
-
-    servo0.attach(10);
-    servo1.attach(9);
-    servo2.attach(8);
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+  devStatus = mpu.dmpInitialize();
+  mpu.setXGyroOffset(1);
+  mpu.setYGyroOffset(-35);
+  mpu.setZGyroOffset(-21);
+  mpu.setZAccelOffset(1749); 
+  if (devStatus == 0) {
+    mpu.setDMPEnabled(true);
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+    dmpReady = true;
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+  }
+  servo0.attach(10);
+  servo1.attach(9);
+  servo2.attach(8);
 }
 
 void loop() {
-    if (!dmpReady) return;
+  if (!dmpReady) return;
 
-    while (!mpuInterrupt && fifoCount < packetSize) {
-        if (mpuInterrupt && fifoCount < packetSize) {
-          fifoCount = mpu.getFIFOCount();
-        }
+  while (!mpuInterrupt && fifoCount < packetSize) {
+    if (mpuInterrupt && fifoCount < packetSize) {
+      fifoCount = mpu.getFIFOCount();
     }
+  }
 
-    mpuInterrupt = false;
-    mpuIntStatus = mpu.getIntStatus();
-    fifoCount = mpy.getFIFOCount();
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
 
-    if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
-        mpu.resetFIFO();
-        fifoCount = mpu.getFIFOCount(); 
-        Serial.println(F("FIFO overflow!"));
+  fifoCount = mpu.getFIFOCount();
 
-    } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
-        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-        mpu.getFIFOBytes(fifoBuffer, packetSize);
-        fifoCount -= packetSize;
+  if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
+    mpu.resetFIFO();
+    fifoCount = mpu.getFIFOCount();
+    Serial.println(F("FIFO overflow!"));
 
-        mpu.dmpGetQuaternion(&q, fifoBuffer);
-        mpu.dmpGetGravity(&gravity, &q);
-        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-        
-        if (j <= 300) {
-            correct = ypr[0] * 180 / M_PI; 
-            j++;
-            servo0.write(90);
-            servo1.write(90);
-            servo2.write(90);
-        }
-        else {
-            float yaw = (ypr[0] * 180 / M_PI) - correct;
-            float pitch = ypr[1] * 180 / M_PI;
-            float roll = ypr[2] * 180 / M_PI;
+  } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+    fifoCount -= packetSize;
 
-            int servo0Value = map(yaw, -90, 90, 180, 0);   // Yaw
-            int servo1Value = map(pitch, -90, 90, 180, 0); // Pitch
-            int servo2Value = map(roll, -90, 90, 0, 180);  // Roll
+#ifdef OUTPUT_READABLE_YAWPITCHROLL
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
 
-            servo0.write(servo0Value);
-            servo1.write(servo1Value);
-            servo2.write(servo2Value);
-        }
+    ypr[0] = ypr[0] * 180 / M_PI;
+    ypr[1] = ypr[1] * 180 / M_PI;
+    ypr[2] = ypr[2] * 180 / M_PI;
+    
+    if (j <= 300) {
+      correct = ypr[0]; // Yaw starts at random value, so we capture last value after 300 readings
+      j++;
     }
+    else {
+      ypr[0] = ypr[0] - correct; 
+      int servo0Value = map(ypr[0], -90, 90, 0, 180); //yaw
+      int servo1Value = map(ypr[1], -90, 90, 180, 0); //pitch
+      int servo2Value = map(ypr[2], -90, 90, 0, 180); //roll
+      
+      servo0.write(servo0Value);
+      servo1.write(servo1Value);
+      servo2.write(servo2Value);
+    }
+#endif
+  }
 }
